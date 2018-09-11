f2901236b96a1ce4c892f3b844574cb938d5f183
alecdeitz <alecdeitz@gmail.com>
2018-09-11 08:23:19 -0400

    Added files

---
+++ VstsBasicApp/VstsBasicApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VstsBasicApp.Startup))]
+namespace VstsBasicApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
